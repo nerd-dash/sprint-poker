90e7d052f267dc8185417c86b99b4746d28c4ea3
Flávio Barcelos <arantesbarcelos@gmail.com>
2025-12-31 14:38:39 +0000

    chore: replace .eslintrc.cjs with .eslintrc.json for ESLint configuration

--- .eslintrc.cjs
+++
@@ -1,4 +0,0 @@
-// Compatibility wrapper for environments that don't detect the flat `eslint.config.js`.
-// Some CI runners and older integrations look for a traditional config file like
-// `.eslintrc.*`. Re-export the flat config so both setups work.
-module.exports = require('./eslint.config.js');
